d5a2367b81790b7a6d8d7cbdd1eaab51bee1ff55
Andrew <muszynskiandrew@uri.edu>
2025-10-06 11:06:07 -0400

    Add HTML template and image download functinality

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, send_file
+import socket
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to the Raspberry Pi web application!'
+    hostname = socket.gethostname()
+    return render_template('index.html', hostname=hostname)
+
+@app.route('/about')
+def about():
+    return '''
+    <h1>About This Server</h1>
+    <p>This is a Raspberry Pi 5 running Flask!</p>
+    <p><a href="/">Back to Home</a></p>
+    '''
+
+@app.route('/download')
+def download_file():
+    return send_file('static/raspberry_pi.png', 
+                     as_attachment=True,
+                     download_name='raspberry_pi_image.png')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
